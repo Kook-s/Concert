40238c3de86e2afcd683f8dd40c1864ad43ed8d6
Kook-s <kooks1126@naver.com>
2025-06-14 22:32:33 +0900

    feat : user,point

--- src/main/java/io/concert/interfaces/PointController.java
+++ src/main/java/io/concert/interfaces/PointController.java
@@ -1,2 +1,40 @@
-package io.concert.interfaces;public class PointController {
+package io.concert.interfaces;
+
+import io.concert.domain.model.Point;
+import io.concert.domain.model.User;
+import io.concert.domain.service.PointService;
+import io.concert.domain.service.UserService;
+import io.concert.interfaces.dto.PointResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/point")
+@RequiredArgsConstructor
+public class PointController {
+
+    private final PointService pointService;
+    private final UserService userService;
+
+
+    @GetMapping
+    public ResponseEntity<PointResponse> getPoint(@RequestParam long userId) {
+        User findUser = userService.getUser(userId);
+        Point findPoint = pointService.getPoint(findUser.id());
+
+        return ResponseEntity.ok(PointResponse.from(findPoint));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> chargePoint(
+            @RequestParam long userId,
+            @RequestParam int amount
+    ) {
+        User findUser = userService.getUser(userId);
+        pointService.chargePoint(findUser.id(), amount);
+
+        return ResponseEntity.ok("Success");
+    }
+
 }
